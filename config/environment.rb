8c1aa7f302088b043b8b7f99a8f2371074ac8104
Marti D <me@martidolce.com>
2021-03-14 16:14:53 -0400

    Added My Custom CSS + Material Theme CSS Created listings_controller.rb and Listings Table Updated .env file Added Counter to Seed file, but need to print on one line NEXT: Seed table with full address using Faker::Local.config, test Listings CRUD

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,6 @@
-ENV['SINATRA_ENV'] ||= "development"
+# frozen_string_literal: true
+
+ENV['SINATRA_ENV'] ||= 'development'
 require 'bundler/setup'
 require 'mysql2'
 require 'dotenv'
@@ -6,15 +8,15 @@ Dotenv.load
 
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger = Logger.new($stdout)
 
 configure :development do
   ActiveRecord::Base.establish_connection(
-    :adapter => "mysql2",
-    :encoding => "utf8",
-    :database => "dev_twenty45_local",
-    :username => "root",
-    :password => "Palo5non!"
+    adapter: 'mysql2',
+    encoding: 'utf8',
+    database: 'dev_twenty45',
+    username: 'root',
+    password: 'Palo5non!'
   )
 end
 
@@ -22,4 +24,4 @@ configure :production do
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'mysql://localhost/mydb')
 end
 
-# require_all 'app'
+require_all 'app'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,32 @@
-require 'faker'
+# frozen_string_literal: true
 
-percent = 0
-cnt_listings = 25
-cnt_listings.times do
-  cnt_listings.each{ |x| sleep(0.05); print "#{percent += 1}%\r" }
-
+puts 'Adding Listings'
+num = 0
+30.times do
+  percent = num / 30 * 100
+  sleep(0.05)
+  print "...#{num += 1} seconds remaining\r"
   Listing.create({
-                   "name" => Faker::Name.unique.name,
-                   "tag_name" => Faker::Marketing.buzzwords,
-                   "status" => "Active", # Active/Inactive Faker?
-                   "first_listed" => Faker::Date.between(from: '2001-03-11', to: '2021-03-11'),
-                   "bedrooms" => Faker.Number.between(from: 1, to: 5) + "BR",
-                   "bathrooms" => Faker::Number.between(from: 1, to: 5) + "BA",
-                   "summary" => Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
-                   "sqft" => Faker::Number.between(from: 900, to: 6000) + "sq. ft.",
-                   "asking_price" => Faker::Commerce.price(range: 10_000..30_000_000.0, as_string: true),
-                   "photo_1" => Faker::LoremFlickr.image(size: "320x240", search_terms: ['homes_for_sale?random=1']),
-                   "photo_2" => Faker::LoremFlickr.image(size: "320x240", search_terms: ['homes_for_sale?random=2']),
-                   "photo_3" => Faker::LoremFlickr.image(size: "320x240", search_terms: ['homes_for_sale?random=3']),
-                   "photo_4" => Faker::LoremFlickr.image(size: "320x240", search_terms: ['homes_for_sale?random=4']),
-                   "user_id" => User.ids.sample,
-                   "phone" => Faker::PhoneNumber.unique.cell_phone,
-                   "community" => "Homely"
+                   'name' => Faker::Address.street_name,
+                   'tag_name' => Faker::Marketing.buzzwords,
+                   'status' => "#{(1..2).to_a.sample}", # Active/Inactive Faker?
+                   'first_listed' => Faker::Date.between(from: '2001-03-11', to: '2021-03-14'),
+                   'bedrooms' => "#{(1..5).to_a.sample} BR",
+                   'bathrooms' => "#{(1..5).to_a.sample} BA",
+                   'summary' => Faker::Lorem.paragraph(sentence_count: 2, supplemental: false,
+                                                       random_sentences_to_add: 4),
+                   'sqft' => "#{(900..6000).to_a.sample} sq. ft.",
+                   # 'asking_price' => Faker::Commerce.price(range: 10_000..30_000_000, as_string: true),
+                   'asking_price' => "$#{(10_000..30_000_000).to_a.sample}",
+                   'photo_1' => Faker::LoremFlickr.image(size: '320x240', search_terms: ['homes_for_sale?random=1']),
+                   'photo_2' => Faker::LoremFlickr.image(size: '320x240', search_terms: ['homes_for_sale?random=2']),
+                   'photo_3' => Faker::LoremFlickr.image(size: '320x240', search_terms: ['homes_for_sale?random=3']),
+                   'photo_4' => Faker::LoremFlickr.image(size: '320x240', search_terms: ['homes_for_sale?random=4']),
+                   # "user_id" => User.ids.sample,
+                   'phone' => Faker::PhoneNumber.unique.cell_phone,
+                   'community' => 'Homely'
                  })
+  num += 1
+
+  system('clear')
 end
